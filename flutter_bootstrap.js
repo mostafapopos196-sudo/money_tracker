eb58a47a6dce5b6e56c58e7320ddc6a195df64c5
oday <mostafapopos196@gmail.com>
2026-07-20 21:27:37 +0300

    force update web files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1148695341" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "442670349" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
